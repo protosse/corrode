13f0c7c6105dc2d438d135163e5953713465beb5
protosse <liuliu1994@gmail.com>
2021-03-27 00:40:47 +0800

    :tada:

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:corrode/pages/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  runApp(GetMaterialApp(
+    home: HomePage(),
+  ));
+}
