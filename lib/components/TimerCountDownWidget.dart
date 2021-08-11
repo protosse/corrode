76adeaae8ea35d7db9e41e68487e0880a961dd15
protosse <liuliu1994@gmail.com>
2021-08-11 09:17:24 +0800

    fix borderRadius topLeft topRight

--- lib/components/TimerCountDownWidget.dart
+++ lib/components/TimerCountDownWidget.dart
@@ -1,51 +1,36 @@
 import 'dart:async';
 
-import 'package:corrode/util/extensions/color_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class TimerCountDownWidget extends StatefulWidget {
-  final Function onTimerFinish;
+typedef TimerCountDownBuilder = Widget Function(
+    TimerCountDownController controller);
 
-  TimerCountDownWidget({required this.onTimerFinish}) : super();
+class TimerCountDownWidget extends StatelessWidget {
+  final VoidCallback? onTimerFinish;
+  final TimerCountDownBuilder builder;
 
-  @override
-  State<StatefulWidget> createState() => TimerCountDownWidgetState();
-}
-
-class TimerCountDownWidgetState extends State<TimerCountDownWidget> {
-  Timer? _timer;
-  var countdownTime = 0.obs;
+  TimerCountDownWidget({this.onTimerFinish, required this.builder});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        if (countdownTime.value == 0) {
-          countdownTime.value = 60;
-          startCountdownTimer();
-        }
-      },
-      child: Obx(() => Text(
-            countdownTime > 0 ? '$countdownTime秒后获取' : "获取验证码",
-            style: TextStyle(fontSize: 12),
-          )),
-      style: TextButton.styleFrom(
-        backgroundColor: HexColor.fromHex("#03C19B"),
-        primary: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(21),
-          ),
-        ),
-      ),
-    );
+    final c = Get.put(TimerCountDownController());
+    c.onTimerFinish = onTimerFinish;
+    return builder(c);
   }
+}
+
+class TimerCountDownController extends GetxController {
+  Timer? _timer;
+  var countdownTime = 0.obs;
+  VoidCallback? onTimerFinish;
 
   void startCountdownTimer() {
     _timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
       if (countdownTime < 1) {
-        widget.onTimerFinish();
+        if (onTimerFinish != null) {
+          onTimerFinish!();
+        }
         _timer?.cancel();
       } else {
         countdownTime = countdownTime - 1;
@@ -54,8 +39,8 @@ class TimerCountDownWidgetState extends State<TimerCountDownWidget> {
   }
 
   @override
-  void dispose() {
-    super.dispose();
+  void onClose() {
     _timer?.cancel();
+    super.onClose();
   }
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -14,145 +14,164 @@ class LoginPage extends GetView<LoginController> {
             title: Text("登录"),
           ),
           body: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 10,
-                  color: HexColor.fromHex("#F4F8F7"),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(24),
-                        topRight: Radius.circular(24),
-                      )),
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 40, right: 40, top: 40),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 15, bottom: 6),
-                            child: Text(
-                              "手机号",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: HexColor.fromHex("#3E5752"),
-                              ),
+            child: Container(
+              color: HexColor.fromHex("#F4F8F7"),
+              padding: EdgeInsets.only(top: 10),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    )),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15, bottom: 6),
+                          child: Text(
+                            "手机号",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: HexColor.fromHex("#3E5752"),
                             ),
                           ),
-                          Container(
-                            height: 40,
-                            decoration: BoxDecoration(
-                              color: HexColor.fromHex("#EFF5F4"),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(21)),
-                            ),
-                            padding: const EdgeInsets.symmetric(horizontal: 15),
-                            child: TextField(
-                              controller: controller.phoneController,
-                              style: TextStyle(fontSize: 14),
-                              keyboardType: TextInputType.phone,
-                              decoration: InputDecoration(
-                                  hintText: "请输入手机号",
-                                  hintStyle: TextStyle(
-                                    color: HexColor.fromHex("#426B63")
-                                        .withOpacity(0.3),
-                                  ),
-                                  enabledBorder: InputBorder.none),
-                            ),
+                        ),
+                        Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: HexColor.fromHex("#EFF5F4"),
+                            borderRadius: BorderRadius.all(Radius.circular(21)),
                           ),
-                          SizedBox(height: 30),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 15, bottom: 6),
-                            child: Text(
-                              "验证码",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: HexColor.fromHex("#3E5752"),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            height: 40,
-                            decoration: BoxDecoration(
-                              color: HexColor.fromHex("#EFF5F4"),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(21)),
-                            ),
-                            padding: const EdgeInsets.symmetric(horizontal: 15),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: TextField(
-                                    controller: controller.codeController,
-                                    style: TextStyle(fontSize: 14),
-                                    keyboardType: TextInputType.phone,
-                                    decoration: InputDecoration(
-                                      hintText: "请输入验证码",
-                                      hintStyle: TextStyle(
-                                        color: HexColor.fromHex("#426B63")
-                                            .withOpacity(0.3),
-                                      ),
-                                      enabledBorder: InputBorder.none,
-                                    ),
-                                  ),
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: TextField(
+                            controller: controller.phoneController,
+                            style: TextStyle(fontSize: 14),
+                            keyboardType: TextInputType.phone,
+                            decoration: InputDecoration(
+                                hintText: "请输入手机号",
+                                hintStyle: TextStyle(
+                                  color: HexColor.fromHex("#426B63")
+                                      .withOpacity(0.3),
                                 ),
-                                Container(
-                                  height: 32,
-                                  child: TimerCountDownWidget(
-                                    onTimerFinish: () {},
-                                  ),
-                                )
-                              ],
+                                enabledBorder: InputBorder.none),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15, bottom: 6),
+                          child: Text(
+                            "验证码",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: HexColor.fromHex("#3E5752"),
                             ),
                           ),
-                          SizedBox(height: 90),
-                          Row(
+                        ),
+                        Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: HexColor.fromHex("#EFF5F4"),
+                            borderRadius: BorderRadius.all(Radius.circular(21)),
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Row(
                             children: [
                               Expanded(
-                                child: Container(
-                                  height: 44,
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    child: Text(
-                                      "登录",
-                                      style: TextStyle(
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.bold),
+                                child: TextField(
+                                  controller: controller.codeController,
+                                  style: TextStyle(fontSize: 14),
+                                  keyboardType: TextInputType.phone,
+                                  decoration: InputDecoration(
+                                    hintText: "请输入验证码",
+                                    hintStyle: TextStyle(
+                                      color: HexColor.fromHex("#426B63")
+                                          .withOpacity(0.3),
                                     ),
-                                    style: TextButton.styleFrom(
-                                      backgroundColor:
-                                          HexColor.fromHex("#03C19B"),
-                                      primary: Colors.white,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(
-                                          Radius.circular(21),
-                                        ),
+                                    enabledBorder: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                              _codeButton()
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 90),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 44,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "登录",
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  style: TextButton.styleFrom(
+                                    backgroundColor:
+                                        HexColor.fromHex("#03C19B"),
+                                    primary: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(21),
                                       ),
                                     ),
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
           ),
         );
       },
+    );
+  }
+
+  Widget _codeButton() {
+    return Container(
+      height: 32,
+      child: TimerCountDownWidget(
+        builder: (c) {
+          return ElevatedButton(
+            onPressed: () {
+              if (c.countdownTime.value == 0) {
+                c.countdownTime.value = 60;
+                c.startCountdownTimer();
+              }
+            },
+            child: Obx(() => Text(
+                  c.countdownTime > 0 ? '${c.countdownTime}秒后获取' : "获取验证码",
+                  style: TextStyle(fontSize: 12),
+                )),
+            style: TextButton.styleFrom(
+              backgroundColor: HexColor.fromHex("#03C19B"),
+              primary: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(21),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
