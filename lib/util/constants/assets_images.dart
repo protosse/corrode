beff5103b49952a2ab267e1f4e25330c94d5a41a
protosse <liuliu1994@gmail.com>
2021-08-11 22:51:40 +0800

    asset fix

--- lib/util/constants/assets_images.dart
+++ lib/util/constants/assets_images.dart
@@ -11,4 +11,32 @@ class AssetImages {
   /// Assets for readBg
   /// read_bg
   static  AssetImage get readBg =>  AssetImage(Assets.readBg);
+
+  /// Assets for tabIcBookcase
+  /// tab/ic_bookcase
+  static  AssetImage get tabIcBookcase =>  AssetImage(Assets.tabIcBookcase);
+
+  /// Assets for tabIcBookcaseS
+  /// tab/ic_bookcase_s
+  static  AssetImage get tabIcBookcaseS =>  AssetImage(Assets.tabIcBookcaseS);
+
+  /// Assets for tabIcBookstore
+  /// tab/ic_bookstore
+  static  AssetImage get tabIcBookstore =>  AssetImage(Assets.tabIcBookstore);
+
+  /// Assets for tabIcBookstoreS
+  /// tab/ic_bookstore_s
+  static  AssetImage get tabIcBookstoreS =>  AssetImage(Assets.tabIcBookstoreS);
+
+  /// Assets for tabIcFound
+  /// tab/ic_found
+  static  AssetImage get tabIcFound =>  AssetImage(Assets.tabIcFound);
+
+  /// Assets for tabIcPersonalcenter
+  /// tab/ic_personalcenter
+  static  AssetImage get tabIcPersonalcenter =>  AssetImage(Assets.tabIcPersonalcenter);
+
+  /// Assets for tabIcPersonalcenterS
+  /// tab/ic_personalcenter_s
+  static  AssetImage get tabIcPersonalcenterS =>  AssetImage(Assets.tabIcPersonalcenterS);
 }
